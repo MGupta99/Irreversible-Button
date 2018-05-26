863f2a2ca9af4e7c0e01293bed0aa6f9fab3defe
Milan Gupta <milagu@umich.edu>
2018-05-25 22:53:14 -0400

    reset database to 0

--- count.php
+++ count.php
@@ -1,17 +1,5 @@
 <?php
   $count = intval($_POST['value']);
-  /*$db = new SQLite3('data.db');
-  if ($count == 0) {
-    $db->exec('CREATE TABLE IF NOT EXISTS count (tally INTEGER PRIMARY KEY)');
-    if (is_null(($db->querySingle('SELECT tally FROM count')))){
-      $db->exec('INSERT OR REPLACE INTO count (tally) VALUES (0)');
-    }
-    $result = $db->query('SELECT tally FROM count');
-    echo strip_tags(($result->fetchArray(SQLITE3_NUM))[0]);
-  } else {
-      $db->exec("UPDATE count SET tally = '$count'");
-      echo 'Databse Updated with Count = ' . $count;
-  }*/
 
   $db_name = 'data.db';
   $db_drvr = 'sqlite';
@@ -24,7 +12,7 @@
     $new_count = $result->fetch(PDO::FETCH_NUM);
     echo strip_tags($new_count[0]);
 
-  // Updates database if button pressed 
+  // Updates database if button pressed
   } else {
     $pdo->exec("UPDATE count SET tally = '$count'");
     echo 'Database Updated with Count = ' . $count;
